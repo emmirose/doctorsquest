49d0bf9d1e9a68e20de478e5592d652c0e99a0b4
Emma Rosso <emmarosso.pro@gmail.com>
2023-06-28 14:54:54 +0200

    doctors quest

---
+++ src/main/java/com/doctors/doctorsquest/DoctorsquestApplication.java
@@ -0,0 +1,13 @@
+package com.doctors.doctorsquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doctors/doctorsquest/DoctorsquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctors.doctorsquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
